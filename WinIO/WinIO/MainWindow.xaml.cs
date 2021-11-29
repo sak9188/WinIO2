0d26ffe24b688869ac98ab032eb13ce679cfb205
HK <sak9188@163.com>
2021-11-29 11:32:55 +0800

    整合消息栏通知

--- WinIO/WinIO/MainWindow.xaml.cs
+++ WinIO/WinIO/MainWindow.xaml.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -21,9 +23,38 @@ namespace WinIO
     /// </summary>
     public partial class MainWindow : AcrylicWindow
     {
+        #region Field
+
+        private readonly NotifyIcon _notifyIcon;
+
+        #endregion
+
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+            // notifyIcon
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            _notifyIcon.BalloonTipClosed += (s, e) => _notifyIcon.Visible = false;
+        }
+
+        private void Notification(int time, string title, string context, ToolTipIcon icon = ToolTipIcon.None)
+        {
+            _notifyIcon.Visible = true;
+            _notifyIcon.ShowBalloonTip(time, title, context, icon);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            TestShowNotification();
+        }
+
+
+        private void TestShowNotification()
+        {
+            Notification(1000, "fuck:", "test");
         }
     }
 }
